9f9f05a141aba6dc2201612255ba6329b90ca411
hyesu33 <kdjsksjchjakqosid@gmail.com>
2025-06-03 23:05:09 +0900

    Update class.py

--- class.py
+++ class.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🎬 영어 수업용 영상")
+st.title("🎬 What Is the Passive Voice?")
 
 # 유튜브 영상 삽입
 st.video("https://youtu.be/JDuMljo0uik?si=24o-5IQ_6--tysv-")
 
-st.write("위 영상은 영어 수업 중 활용될 수 있는 자료입니다.")
+st.write("Watch this if you want to learn more about the passive voice!")
